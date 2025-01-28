794619064c6a2dc10ebea97e52d0c8e78b39cef2
joshweinstock <jyweinstock@uchicago.edu>
2025-01-28 00:05:06 +0000

    lucky6

--- lucky_number.rb
+++ lucky_number.rb
@@ -16,9 +16,7 @@ get("/dice/2/6")do
 first_die = rand(1..6)
 second_die = rand(1..6)
 sum = first_die + second_die
-
 @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
 erb(:two_six, { :layout=>:wrapper})
 end
 
@@ -26,20 +24,16 @@ get("/dice/2/10") do
   first_die=rand(1..10)
   second_die=rand(1..10)
   sum=first_die+second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
-  erb{ :layout => :wrapper }
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_ten, {:layout => :wrapper })
 end
 
 get("/dice/2/20") do
   first_die = rand(1..20)
   second_die = rand(1..20)
   sum=first_die+second_die
-  outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  "<h1>2d20</h1>
-  <p>#{outcome}</p>"
-  erb{ :layout => :wrapper }
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_twenty, {:layout => :wrapper })
 end
 
 get("/dice/5/4") do
@@ -49,8 +43,6 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   fifth_die = rand(1..4)
   sum=first_die+second_die+third_die+fourth_die+fifth_die
-  outcome = "you rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die}, for a total of #{sum}."
-  "<h1>2d20</h1>
-  <p>#{outcome}</p>"
-  erb{ :layout => :wrapper }
+  @outcome = "you rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die}, for a total of #{sum}."
+  erb(:five_four, {:layout => :wrapper })
 end
